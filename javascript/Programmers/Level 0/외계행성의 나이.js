393a87e3dba4f873daf5081955c99e037d836639
Gyumin Lee <gyumin.q.lee@gmail.com>
2022-12-08 00:58:27 +0900

    [221208][Level 0] 외계행성의 나이, split

---
+++ javascript/Programmers/Level 0/외계행성의 나이.js
@@ -0,0 +1,20 @@
+function solution(age) {
+  let alphabet = {
+    0: "a",
+    1: "b",
+    2: "c",
+    3: "d",
+    4: "e",
+    5: "f",
+    6: "g",
+    7: "h",
+    8: "i",
+    9: "j",
+  };
+
+  let newAge = age + "";
+  return newAge
+    .split("")
+    .map((el) => alphabet[el])
+    .join("");
+}
