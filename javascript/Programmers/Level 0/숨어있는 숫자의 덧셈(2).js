2670424ea64cccfcdd3d486315f833ccd6904b17
Gyumin Lee <gyumin.q.lee@gmail.com>
2022-12-12 01:05:29 +0900

    [221212][Level 0] 숨어있는 숫자의 덧셈(2), isNaN

--- javascript/Programmers/Level 0/숨어있는 숫자의 덧셈(2).js
+++ javascript/Programmers/Level 0/숨어있는 숫자의 덧셈(2).js
@@ -1,9 +1,7 @@
-문자열 my_string이 매개변수로 주어집니다. 
-my_string은 소문자, 대문자, 자연수로만 구성되어있습니다. 
-my_string안의 자연수들의 합을 return하도록 solution 함수를 완성해주세요.
-
-
-
+// 문자열 my_string이 매개변수로 주어집니다.
+// my_string은 소문자, 대문자, 자연수로만 구성되어있습니다.
+// my_string안의 자연수들의 합을 return하도록 solution 함수를 완성해주세요.
+// 연속된 수는 하나의 숫자로 간주합니다.
 
 function solution(my_string) {
   let result = 0;
