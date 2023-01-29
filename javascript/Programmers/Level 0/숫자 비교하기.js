704d00d40b08c133316ddfb8511b4e97484d7983
Gyumin Lee <gyumin.q.lee@gmail.com>
2023-01-29 23:36:41 +0900

    [230129][Level 0] 숫자 비교하기, 삼항연산자

---
+++ javascript/Programmers/Level 0/숫자 비교하기.js
@@ -0,0 +1,12 @@
+// if
+function solution(num1, num2) {
+  if (num1 === num2) {
+    return 1;
+  }
+  return -1;
+}
+
+// 삼항연산자(ternary operator)
+function solution(num1, num2) {
+  return num1 === num2 ? 1 : -1;
+}
