d5965f405e8bfc017e34ea8b34d19d5745a7234c
Gyumin Lee <gyumin.q.lee@gmail.com>
2023-02-03 18:50:20 +0900

    [230203][Level 0] 컨트롤 제트, 전개 연산자

---
+++ javascript/Programmers/Level 0/컨트롤 제트.js
@@ -0,0 +1,13 @@
+function solution(s) {
+  const arr = [...s.split(" ")];
+
+  var answer = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "Z") {
+      answer -= Number(arr[i - 1]);
+    } else {
+      answer += Number(arr[i]);
+    }
+  }
+  return answer;
+}
