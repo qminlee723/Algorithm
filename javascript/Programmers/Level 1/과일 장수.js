8d54779c1b279f821a76dc41a232740ff9cc5a7a
Gyumin Lee <gyumin.q.lee@gmail.com>
2023-02-23 23:26:58 +0900

    [230223][Level 1] 과일 장수, sort()

---
+++ javascript/Programmers/Level 1/과일 장수.js
@@ -0,0 +1,10 @@
+function solution(k, m, score) {
+  var answer = 0;
+
+  score.sort((a, b) => b - a);
+
+  for (let i = m - 1; i < score.length; i += m) {
+    answer += score[i] * m;
+  }
+  return answer;
+}
