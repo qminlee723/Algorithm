13f87d63c9ba4a5786141a88254e90f5e0132824
Gyumin Lee <gyumin.q.lee@gmail.com>
2023-03-29 19:28:03 +0900

    [230329][Binary Search] #704. Binary Search

--- javascript/LeetCode/Algorithm I/704. Binary Search.js
+++ javascript/LeetCode/Algorithm I/704. Binary Search.js
@@ -3,6 +3,27 @@
 // If target exists, then return its index. Otherwise, return -1.
 // You must write an algorithm with O(log n) runtime complexity.
 
+// var search = function (nums, target) {
+//   return nums.indexOf(target);
+// };
+
 var search = function (nums, target) {
-  return nums.indexOf(target);
+  let start = 0;
+  let end = nums.length - 1;
+  let index = 0;
+
+  while (end >= start) {
+    let mid = Math.floor((start + end) / 2);
+    if (target === nums[mid]) {
+      index = mid;
+      return index;
+    } else if (target > nums[mid]) {
+      start = mid + 1;
+      index = start + 1;
+    } else {
+      end = mid - 1;
+      index = end + 1;
+    }
+  }
+  return -1;
 };
