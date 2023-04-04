1d9419750ae89d8f9acc7e5e572e5f8818af7baf
Gyumin Lee <gyumin.q.lee@gmail.com>
2023-04-04 19:25:48 +0900

    [230404][Sliding Window] #3. ...add comments

--- javascript/LeetCode/Algorithm I/3. Longest Substring Without Repeating Characters.js
+++ javascript/LeetCode/Algorithm I/3. Longest Substring Without Repeating Characters.js
@@ -3,6 +3,7 @@ var lengthOfLongestSubstring = function (s) {
   let left = 0;
   let maxSize = 0;
 
+  // s의 길이가 0이면 0을, 1이면 1을 반환한다
   if (s.length === 0) {
     return 0;
   } else if (s.length === 1) {
@@ -11,11 +12,12 @@ var lengthOfLongestSubstring = function (s) {
 
   for (let i = 0; i < s.length; i++) {
     while (set.has(s[i])) {
-      set.delete(s[left]);
-      left++;
+      //2. set에 다음 알파벳이 있다면, 즉 중복된다면
+      set.delete(s[left]); //젤 왼쪽에 있는 값을 지워주고
+      left++; // left 인덱스를 올려줍니다
     }
-    set.add(s[i]);
-    maxSize = Math.max(maxSize, i - left + 1);
+    set.add(s[i]); //1. set에 알파벳 하나씩을 순서대로 넣기
+    maxSize = Math.max(maxSize, i - left + 1); //최댓값구하기: i, left 둘 다 인덱스라 1을 더해줌
   }
   return maxSize;
 };
