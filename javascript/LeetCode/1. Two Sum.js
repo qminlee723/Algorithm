1d51c8586deb8e542af711d8feb5161127ea8284
Gyumin Lee <gyumin.q.lee@gmail.com>
2023-04-11 21:50:47 +0900

    [230411][hashmap] #1. Two Sum

--- javascript/LeetCode/1. Two Sum.js
+++ javascript/LeetCode/1. Two Sum.js
@@ -1,3 +1,30 @@
+// hashmap
+var twoSum = function (nums, target) {
+  let map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(target - nums[i])) {
+      // 더했을 때 타겟이 되는 숫자(즉, 타겟-현재숫자)가 해쉬맵에 있으면
+      return [i, map.get(target - nums[i])];
+    }
+    map.set(nums[i], i); // key: 숫자, value: 인덱스
+  }
+};
+
+// double for loop
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (i !== j) {
+        if (nums[i] + nums[j] === target) {
+          return [i, j];
+        }
+      }
+    }
+  }
+};
+
+// two pointer
 var twoSum = function (nums, target) {
   let left = 0;
   let right = left + 1;
