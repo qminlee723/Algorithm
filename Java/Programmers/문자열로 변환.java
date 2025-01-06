640d5e9b39e36bf791bf83d1bc845e953acd880e
Gyumin Lee <gyumin.q.lee@gmail.com>
2025-01-07 01:53:56 +0900

    240107 toString()

---
+++ Java/Programmers/문자열로 변환.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(int n) {
+
+        // 문자열로 변환 메서드
+        // Integer.toString()
+        // "" + Integer -> String
+
+        // String answer = Integer.toString(n);
+        String answer = "" + n;
+        return answer;
+    }
+}
