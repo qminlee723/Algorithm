371e7b113ac7d615f01e97c26ad5f50b1b092edd
Gyumin Lee <gyumin.q.lee@gmail.com>
2025-01-09 01:58:55 +0900

    240109 특수문자 출력하기

---
+++ Java/Programmers/특수문자 출력하기.java
@@ -0,0 +1,38 @@
+logo
+코딩테스트 연습
+코딩 기초 트레이닝
+특수문자 출력하기
+도움말
+컴파일 옵션
+특수문자 출력하기
+제출 내역
+문제 설명
+다음과 같이 출력하도록 코드를 작성해 주세요.
+
+출력 예시
+
+!@#$%^&*(\'"<>?:;
+Solution.java
+\
+1
+import java.util.Scanner;
+2
+​
+3
+public class Solution {
+4
+    public static void main(String[] args) {
+5
+        System.out.println("!@#$%^&*(\\'\"<>?:;");
+6
+    }
+7
+}
+실행 결과
+채점을 시작합니다.
+정확성  테스트
+테스트 1 〉	통과 (128.97ms, 64.7MB)
+채점 결과
+정확성: 100.0
+합계: 100.0 / 100.0
+프로그래머스 K-Digital Training
