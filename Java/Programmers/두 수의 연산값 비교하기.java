7788ca186cabe15c5c606beec236402735b75962
Gyumin Lee <gyumin.q.lee@gmail.com>
2025-01-10 00:35:21 +0900

    240110 두 수의 연산값 비교하기

--- Java/Programmers/두 수의 연산값 비교하기.java
+++ Java/Programmers/두 수의 연산값 비교하기.java
@@ -2,6 +2,8 @@ class Solution {
     public int solution(int a, int b) {
         
         String concatStr = a + "" + b + "";
+        // a + "" + b 해도 됨
+
         int multiply = 2 * a * b;
         int concat = Integer.parseInt(concatStr);
         
