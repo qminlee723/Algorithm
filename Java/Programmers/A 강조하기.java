3d52df65758ad500e9f1b442f91f9c6e77781ce1
Gyumin Lee <gyumin.q.lee@gmail.com>
2025-01-28 23:58:48 +0900

    [250128][String] A강조하기

---
+++ Java/Programmers/A 강조하기.java
@@ -0,0 +1,22 @@
+class Solution {
+  public String solution(String myString) {
+      String answer = "";
+      
+      String lowString = myString.toLowerCase();
+      
+      String[] temp = lowString.split("");
+      
+      for (int i=0; i<myString.length(); i++) {
+          
+          // == 대신 equals 사용
+          // == 사용시 참조값 (메모리 주소) 비교함
+          if (temp[i].equals("a")) {
+              temp[i] = "A";
+          } 
+          
+          answer += temp[i];
+      }
+
+      return answer;
+  }
+}
